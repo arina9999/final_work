d4a07c364da14b7ae2fbc4172cb85d25fd872682
arina9999 <aacher9@mail.ru>
2023-01-26 13:12:12 +0600

    third edit commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿string[] FilterArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <=3)
+        {
+            count++;
+        }
+    }
+    string[] newArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        Console.Write($"'{array[i]}, ");
+    }
+    Console.Write($"'{array[array.Length - 1]}']");
+}
+
+string[] myArray =  { "hello", "2", "world", ":-)", "sun", "1567", "-2", "computer scince", "Russia", "Denmark", "Kazan"};
+PrintArray(FilterArray(myArray));
